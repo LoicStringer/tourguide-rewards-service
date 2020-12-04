bc9cca8e43289471c2649e52a886fb54e7df21f6
Loïc Stringer <loic.stringer@gmail.com>
2020-12-04 18:24:20 +0100

    Initial commit

---
+++ src/main/java/com/tourguiderewardsservice/TourguideRewardsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.tourguiderewardsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourguideRewardsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TourguideRewardsServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tourguiderewardsservice/TourguideRewardsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tourguiderewardsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TourguideRewardsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
