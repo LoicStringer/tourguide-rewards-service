796ca4a8fb037b9b41b473d2d55278e1281765ff
Loïc Stringer <loic.stringer@gmail.com>
2020-12-13 11:42:08 +0100

    Added the controller

--- src/main/java/com/tourguiderewardsservice/controller/RewardsServiceController.java
+++ src/main/java/com/tourguiderewardsservice/controller/RewardsServiceController.java
@@ -5,7 +5,7 @@ import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tourguiderewardsservice.service.RewardsService;
@@ -16,9 +16,9 @@ public class RewardsServiceController {
 	@Autowired
 	private RewardsService rewardsService;
 
-	@GetMapping("/rewardPoints")
+	@GetMapping("{userId}/attractions/{attractionId}/reward-points")
 	public ResponseEntity<Integer> getAttractionRewardPoints
-	(@RequestParam("attractionId") UUID attractionId, @RequestParam("userId")UUID userId) {
+	(@PathVariable("userId")UUID userId,@PathVariable("attractionId") UUID attractionId) {
 		return ResponseEntity.ok(rewardsService.getAttractionRewardPoints(attractionId, userId));
 	}
 
