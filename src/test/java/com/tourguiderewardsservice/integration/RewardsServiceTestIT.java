15858af96d6ed8596cfc2fd127b2f3674fb4a8b3
Loïc Stringer <loic.stringer@gmail.com>
2021-01-26 13:08:24 +0100

    Added jacoco plugin

--- src/test/java/com/tourguiderewardsservice/integration/RewardsServiceTestIT.java
+++ src/test/java/com/tourguiderewardsservice/integration/RewardsServiceTestIT.java
@@ -1,5 +1,6 @@
 package com.tourguiderewardsservice.integration;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -15,11 +16,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
+
+import com.tourguiderewardsservice.exception.RewardCentralException;
 import com.tourguiderewardsservice.proxy.RewardCentralProxyImpl;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-class RewardsServiceIT {
+class RewardsServiceTestIT {
 	
 	@Autowired
 	private MockMvc mockMvc;
@@ -35,6 +38,12 @@ class RewardsServiceIT {
 				.andExpect(jsonPath("$").value(100));
 	}
 
-
+	@Test
+	void isExpectedExceptionIsThrownWhenRewardCentralReturnsNull() throws Exception {
+		when(rewardCentralProxyImpl.getAttractionRewardPoints(any(UUID.class), any(UUID.class))).thenThrow(RewardCentralException.class);
+		mockMvc.perform(get("/"+UUID.randomUUID()+"/attractions/"+UUID.randomUUID()+"/reward-points"))
+		.andExpect(status().isNotFound())
+		.andExpect(result->assertTrue(result.getResolvedException() instanceof RewardCentralException));
+	}
 	
 }
